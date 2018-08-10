1d9be533ccce1fe83d24a7a9a230e4132d7423f3
Guilherme Fogo <gui.fogo17@gmail.com>
2018-08-10 11:44:20 -0300

    Update importante.js

--- js/importante.js
+++ js/importante.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
         alert("Sua menagem:"+ campo_nome+" foi enviada");
     });
     $(#erro).click(function()){
-      alert("Caso o donwlod não funciou ou o arquivo não abrir: vá para Redes sociais Clique em github e depois em repositorios explore os projetos")
+      alert("Caso o donwlod não funciou ou o arquivo não abrir: vá para Redes sociais Clique em github e depois em repositorios explore os projetos");
       });
 });
