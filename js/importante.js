8abf58cfa2248e6cfe14ea476f20ecdc2cd8cc9e
Guilherme Fogo <guilherme.fogo17@gmail.com>
2019-08-04 14:10:24 -0300

    adicionando  certificados na plataforma e criando tabela

--- js/importante.js
+++ js/importante.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-    $(".mensagem").click(function(){
-    	var campo_nome = $(".nome").val();
-        alert("Sua menagem:"+ campo_nome+" foi enviada");
-    });
-    $("#erro").click(function () {
-        alert("Caso o donwlod não funciou ou o arquivo não abrir: vá para Redes sociais Clique em github e depois em repositorios explore os projetos");
-	});
+    $(".telefone").text("(11) 971655727");
+    $('.informacoes').text("Para mais informações entre no em contato no Email:")
 });
--- js/meu_vue.js
+++ js/meu_vue.js
@@ -1,10 +1,3 @@
-/*
-Ensino médio (Concluído)
-Senac Santo André - Técnico em Informática (Concluido)
-Victory English Training (Cursando)
-Ensino Superior- Faculdade Metodista 
-(Cursando 4ºsemestre Sistemas de informação)
-*/
 var app = new Vue ({
   el: '#app',
   data: {
@@ -12,7 +5,7 @@ var app = new Vue ({
       {text: 'Ensino médio (Concluído)'},
       {text: 'Senac Santo André - Técnico em Informática (Concluido)'},
       {text: 'Victory English Training (Cursando)'},
-      {text: 'Ensino Superior- Faculdade Metodista (Cursando 5ºsemestre Sistemas de informação)'}
+      {text: 'Ensino Superior- Faculdade Metodista (Cursando 6ºsemestre Sistemas de informação)'}
     ]
   }
 });
@@ -20,7 +13,7 @@ var app = new Vue ({
 var lista_certificados = new Vue({
 	el: '#lista_certificados',
 	 data: {
-     todos: [
+     nome: [
       {text: 'Certificado Gigabyte Curso Multiprofissional'},
       {text: 'Certificado Gigabyte Curso TI Java Script'},
       {text: 'Certificado Gigabyte Curso Hardware e Redes'},
@@ -38,45 +31,44 @@ var lista_certificados = new Vue({
       {text: 'Certificado Sololearn Curso Fundamentos do SQL'},
       {text: 'Certificado Sololearn Curso Fundamentos do Java'},
       {text: 'Certificado Softblue phpacademy 2017'},
-      {text: 'Certificado Alura ITIL Foundation 1:Introdução ao Gerenciamento de TI'},
-      {text: 'Certificado Alura PMP e CAMP parte1: Processos de Gerenciamento de Projetos'}
-
-      
+      {text: 'Certificado Alura PMP e CAMP parte1: Processos de Gerenciamento de Projetos'},
+      {text: 'Certificado Alura PMP E CAPM PARTE 2: Gerenciamento de Integração'},
+      {text: 'Certificado Alura PMP E CAPM PARTE 4: Gerenciamento de Tempo '},
+      {text: 'Certificado Alura Java parte1: Primeiros Passos'},
+      {text: 'Certificado Alura Java parte2: Introdução à Orientação a Objetos'},
+      {text: 'Certificado Alura Java parte5: Pacotes e Java Lang'}
     ]
   }
 });
 
 
-/*
--Certificado Gigabyte Curso Multiprofissional
--Certificado Gigabyte Curso TI Java Script
--Certificado Gigabyte Curso Hardware e Redes
--Certificado Senac Curso Técnico em Informatica
--Certificado Intel(Dialogo TI) Curso Segurança da Informação I
--Certificado Intel(Dialogo TI) Curso Segurança da Informação II
--Certificado Intel(Dialogo TI) Curso Cloud Computing
--Certificado Intel(Dialogo TI) Curso sortware livre
--Certificado BrasilMais TI(Microsoft) Curso desenvolvimento de software
--Certificado BrasilMais TI(Microsoft) Curso Fundamentos da linguagem C#
--Certificado Sololearn Curso Fundamentos do HTML
--Certificado Sololearn Curso Fundamentos do CSS
--Certificado Sololearn Curso Fundamentos do JavaScript
--Certificado Sololearn Curso Fundamentos do PHP
--Certificado Sololearn Curso Fundamentos do SQL
--Certificado Sololearn Curso Fundamentos do Java
--Certificado Softblue phpacademy 2017
-
-*/
-
-
-
-
-
-// var links_certificados = new Vue({
-// 	el: '#links_certificados',
-// 	data: {
-// 		todos:[
-// 			{text: 'img/imagem.png'}
-// 		]
-// 	}
-// });
+var lista_img = new Vue({
+  el: '#lista_img',
+   data: {
+    caminho: [
+       {text: 'Certificado Gigabyte Curso Multiprofissional'},
+       {text: 'Certificado Gigabyte Curso TI Java Script'},
+       {text: 'Certificado Gigabyte Curso Hardware e Redes'},
+       {text: 'Certificado Senac Curso Técnico em Informatica'},
+       {text: 'img/Intel/segurancaInformacao1.PNG'},
+       {text: 'img/Intel/segurancaInformacao2.PNG'},
+       {text: 'Certificado Intel(Dialogo TI) Curso Cloud Computing'},
+       {text: 'img/Intel/SoftwareLivre.PNG'},
+       {text: 'img/BrasilTI/CertificateMicrosoftConseitoBasico.png'},
+       {text: 'Certificado BrasilMais TI(Microsoft) Curso Fundamentos da linguagem C#'},
+       {text: 'img/Sololearn/HTML.jpg'},
+       {text: 'img/Sololearn/CSS.jpg'},
+       {text: 'img/Sololearn/Javascript.jpg'},
+       {text: 'img/Sololearn/PHP.jpg'},
+       {text: 'img/Sololearn/CSS.jpg'},
+       {text: 'img/Sololearn/Java.jpg'},
+       {text: 'img/SoftBlue/PHP2017.PNG'},
+       {text: 'img/GerenciamentoProjetos/PMP1 Processo de gerenciamento de projetos.PNG'},
+       {text: 'img/GerenciamentoProjetos/PMP2 Gerenciamento de Integraçao.PNG'},
+       {text: 'img/GerenciamentoProjetos/pmp4 Gerenciamento de tempo.PNG'},
+       {text: 'img/Java/Java1 primeiros passos.PNG'},
+       {text: 'img/Java/Java2 Orientacao Objetos.PNG'},
+       {text: 'img/Java/Java5 Pacotes e java.lang object.PNG'}
+    ]
+  }
+});
